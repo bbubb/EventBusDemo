2acb65eb23168f51af26bda2ef62d657b9b5b5ca
dremharrison <deandremharrison@gmail.com>
2019-07-18 15:07:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/eventbusdemo/ButtonFragment.java
@@ -0,0 +1,28 @@
+package com.example.eventbusdemo;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ButtonFragment extends Fragment {
+
+
+    public ButtonFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+
+
+        return view;
+    }
+
+}
